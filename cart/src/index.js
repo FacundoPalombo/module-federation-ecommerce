e875fdf1fcb80cb492be916fb68e438a388deb20
Facundo Palombo facundo.palombo@gmail.com <facundo.palombo@gmail.com>
2022-10-18 23:10:17 -0300

    feature: add asynchronous loading modules in microfrontends

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,8 +1,1 @@
-import { faker } from "@faker-js/faker";
-
-let cartText = `<p>You have ${faker.random.numeric()} items in your cart</p>`;
-
-console.log(cartText);
-const body = document.getElementById("dev-cart");
-
-body.innerHTML += cartText;
+import("./bootstrap");
--- products/src/index.js
+++ products/src/index.js
@@ -1,16 +1,1 @@
-import { faker } from "@faker-js/faker";
-
-let products = "";
-
-for (let i = 0; i < 3; i++) {
-  const productName = faker.commerce.productName();
-  products += `<p>${productName}</p>`;
-}
-
-console.log(products);
-const body = document.getElementById("root");
-
-body.innerHTML += products;
-body.style.background = "red";
-body.style.width = "500px";
-body.style.height = "200px";
+import("./bootstrap");
